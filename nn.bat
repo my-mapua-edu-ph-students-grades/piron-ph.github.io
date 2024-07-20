16d5465880e7b58de7b6dc001007ee25dd85e603
Ligolas Neo Malicdem <74213014+piron-ph@users.noreply.github.com>
2024-07-20 01:00:09 -0700

    HAHA
    asfafsa

---
+++ nn.bat
@@ -0,0 +1,79 @@
+@echo off & CD "%~dp0"
+echo "BOBO SI VALL"
+ping localhost -n 2 >nul
+echo "BOBO SI VALL"
+ping localhost -n 2 >nul
+
+echo "BOBO SI VALL"
+ping localhost -n 2 >nul
+echo "BOBO SI VALL"
+ping localhost -n 2 >nul
+
+
+echo "BOBO SI VALL"
+ping localhost -n 2 >nul
+echo "BOBO SI VALL"
+ping localhost -n 2 >nul
+echo "BOBO SI VALL"
+ping localhost -n 2 >nul
+
+echo "BOBO SI VALL"
+ping localhost -n 2 >nul
+echo "BOBO SI VALL"
+ping localhost -n 2 >nul
+echo "BOBO SI VALL"
+ping localhost -n 2 >nul
+
+echo "BOBO SI VALL"
+ping localhost -n 2 >nul
+echo "BOBO SI VALL"
+ping localhost -n 2 >nul
+echo "BOBO SI VALL"
+ping localhost -n 2 >nul
+echo "BOBO SI VALL"
+ping localhost -n 2 >nul
+echo "BOBO SI VALL"
+ping localhost -n 2 >nul
+echo "BOBO SI VALL"
+ping localhost -n 2 >nul
+
+echo "BOBO SI VALL"
+ping localhost -n 2 >nul
+echo "BOBO SI VALL"
+ping localhost -n 2 >nul
+echo "BOBO SI VALL"
+ping localhost -n 2 >nul
+echo "BOBO SI VALL"
+ping localhost -n 2 >nul
+echo "BOBO SI VALL"
+ping localhost -n 2 >nul
+echo "BOBO SI VALL"
+ping localhost -n 2 >nul
+echo "BOBO SI VALL"
+ping localhost -n 2 >nul
+echo "BOBO SI VALL"
+ping localhost -n 2 >nul
+echo "BOBO SI VALL"
+ping localhost -n 2 >nul
+echo "BOBO SI VALL"
+ping localhost -n 2 >nul
+echo "BOBO SI VALL"
+ping localhost -n 2 >nul
+echo "BOBO SI VALL"
+ping localhost -n 2 >nul
+echo "BOBO SI VALL"
+ping localhost -n 2 >nul
+echo "BOBO SI VALL"
+ping localhost -n 2 >nul
+echo "BOBO SI VALL"
+ping localhost -n 2 >nul
+echo "BOBO SI VALL"
+ping localhost -n 2 >nul
+echo "BOBO SI VALL"
+ping localhost -n 2 >nul
+echo "BOBO SI VALL"
+ping localhost -n 2 >nul
+echo "BOBO SI VALL"
+ping localhost -n 2 >nul
+echo "BOBO SI VALL"
+ping localhost -n 2 >nul
